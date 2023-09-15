95069f72954993ee12bb8904818905cd6d7efffb
neuronarchitecture27 <troisiemeoeilagency@gmail.com>
2023-09-15 11:30:13 +0200

    fixing responsive layout v1.4

--- src/components/Header/nav/Footer/index.jsx
+++ src/components/Header/nav/Footer/index.jsx
@@ -1,12 +1,23 @@
 import styles from './style.module.scss';
+import Magnetic from '@/common/Magnetic';
 
 export default function index() {
   return (
     <div className={styles.footer}>
-        <a>Awwwards</a>
-        <a>Instagram</a>
-        <a>Dribble</a>
-        <a>LinkedIn</a>
+      <Magnetic>
+        <a className='footerEl'>Awwwards</a>
+      </Magnetic>
+
+      <Magnetic>
+        <a className='footerEl'>Instagram</a>
+      </Magnetic>
+      <Magnetic>
+        <a className='footerEl'>Dribble</a>
+      </Magnetic>
+      <Magnetic>
+        <a className='footerEl'>LinkedIn</a>
+      </Magnetic>
+
     </div>
   )
 }
