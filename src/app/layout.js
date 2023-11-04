1ea4c93caf1a47a4203be07ac2bc8dacdd2b4dcc
neuronarchitecture27 <troisiemeoeilagency@gmail.com>
2023-11-04 15:02:26 +0100

    fix hydration

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
 import Header from '../components/Header';
+import { Suspense } from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -13,8 +14,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+        <Suspense>
+
         <Header />
         {children}
+        </Suspense>
       </body>
     </html>
   )
