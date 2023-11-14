f7a1548c2555ec5824356433e2021dd0852f02d6
neuronarchitecture27 <troisiemeoeilagency@gmail.com>
2023-11-14 06:53:17 +0100

    fixed menu respon

--- src/components/Heading/index.jsx
+++ src/components/Heading/index.jsx
@@ -6,15 +6,30 @@ import styles from './style.module.scss';
 import Nav from './Nav';
 
 const menu = {
+    // open: {
+    //     width: "23em",
+    //     height: "650px",
+    //     top: "-20px",
+    //     right: "-25px",
+    //     transition: { duration: 0.75, type: "tween", ease: [0.76, 0, 0.24, 1]}
+    // },
+    // closed: {
+    //     width: "6.2em",
+    //     height: "40px",
+    //     top: "0px",
+    //     right: "0px",
+    //     transition: { duration: 0.75, delay: 0.35, type: "tween", ease: [0.76, 0, 0.24, 1]}
+    // }
+    // MOBILE VERSION COMPATIBLE
     open: {
-        width: "480px",
+        width: "23em",
         height: "650px",
-        top: "-25px",
-        right: "-25px",
+        top: "-20px",
+        right: "-5px",
         transition: { duration: 0.75, type: "tween", ease: [0.76, 0, 0.24, 1]}
     },
     closed: {
-        width: "100px",
+        width: "6.2em",
         height: "40px",
         top: "0px",
         right: "0px",
