5c0c0cfe676ff8ca74f2204d757b3245f03b7448
neuronarchitecture27 <troisiemeoeilagency@gmail.com>
2023-11-17 11:14:40 +0100

    adding lazy load v2

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,8 +1,9 @@
 import './globals.css'
 import { Inter } from 'next/font/google'
-import Header from '../components/Header';
-import Heading from '../components/Heading';
-
+// import Header from '../components/Header';
+import { lazy } from "react";
+//inside my components/i have an index.js file 
+export const Header = lazy(()=> import('../components/Header'));
 
 import { Suspense } from 'react';
 
@@ -17,12 +18,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Suspense>
 
         <Header />
         {/* <Heading /> */}
         {children}
-        </Suspense>
       </body>
     </html>
   )
