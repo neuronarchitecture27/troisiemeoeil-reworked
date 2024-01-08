c0d0270bd744c2a136b8dee6b31381c51b871eb0
neuronarchitecture27 <troisiemeoeilagency@gmail.com>
2024-01-08 18:48:32 +0100

    fixing layout issues

--- src/components/Home/Landing/index.jsx
+++ src/components/Home/Landing/index.jsx
@@ -4,6 +4,7 @@ import Sphere from '@/components/Sphere';
 import './index.css'
 import { useEffect, useRef } from "react";
 import styles from './page.module.css'
+import gsap from "gsap";
 
 
 export default function Home() {
@@ -14,8 +15,26 @@ export default function Home() {
   let reqId = null;
 
   useEffect(() => {
+    let headingWrapp = document.querySelector('.ml12')
+    headingWrapp.innerHTML = headingWrapp.textContent.replace(
+      /\S/g,
+      "<span className='letter'>$&</span>"
+    )
+    gsap.timeline().from(".ml12 .letter", {
+        opacity: 0,
+        duration: 0.25,
+        delay: 1,
+        stagger: {
+          amount: 0.5,
+          grid:"auto",
+          from: "random"
+        },
+        ease: "power2.out"
+    })
     setPath(progress);
+
   }, [])
+
 
   const setPath = (progress) => {
     const width = window.innerWidth * 0.48;
@@ -73,7 +92,7 @@ export default function Home() {
             <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="150px" height="150px" ><path d="M17 15.586 6.707 5.293 5.293 6.707 15.586 17H7v2h12V7h-2v8.586z" style={{fill:"#ffffff"}}/></svg>
           </div>
           <div className='headingWrapp'>
-              cutting edge technology.
+             <h1 className="ml12"> cutting edge technology.</h1>
               
               <div className={styles.line}>
               <div 
