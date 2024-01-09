619287d61e12c1aa6f8bf613f1a10b1bbaec2cde
neuronarchitecture27 <troisiemeoeilagency@gmail.com>
2024-01-10 00:41:21 +0100

    fixing drawer

--- src/app/layout.js
+++ src/app/layout.js
@@ -19,10 +19,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en"  >
       <head>
-    <meta 
-    
-          name="viewport"
-          content="width=device-width, initial-scale=1.0" />
+      <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
 </head>
       <body className={inter.className}>
       <link rel="icon" href="/images/favicon.ico" sizes="any" />
