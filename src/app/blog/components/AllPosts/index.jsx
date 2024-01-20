e6e9ef355d9ab01cec5ce62c9156056761ce5243
neuronarchitecture27 <troisiemeoeilagency@gmail.com>
2024-01-20 11:59:43 +0100

    adding skel

--- src/app/blog/components/AllPosts/index.jsx
+++ src/app/blog/components/AllPosts/index.jsx
@@ -8,6 +8,7 @@ import { useEffect, useState } from 'react';
 export default function AllPosts() {
 
   const [posts, setposts] = useState(null)
+  const [isReady, setIsReady] = useState(false);
   useEffect(()=> {
     const getData = async () => {
       let { data: posts, error } = await supabase
@@ -15,6 +16,7 @@ export default function AllPosts() {
       .select()
       .order('created_at', { ascending: false })
       setposts(posts);
+      setIsReady(true);
     console.log(posts);
 
     }
@@ -24,46 +26,98 @@ export default function AllPosts() {
   return (
     <div className=" w-full flex flex-col items-center justify-center h-auto my-5 p-2">
  
-   <div className=" w-4/5 grid lg:grid-cols-3 grid-cols-1 gap-6 px-3 mt-10">
-            {posts && posts.slice(0, 5).map((_, i) => (
+   <div className=" w-4/5 grid lg:grid-cols-3 grid-cols-1 gap-6 px-3 mt-10"> 
+
+   {isReady ? (
+        // Render posts if ready
+        <>
+          {posts && posts.slice(0, 5).map((_, i) => (
            
-                  <div
-                    key={i}
-                    className={`rounded-xl border border-neutral-800 bg-neutral-900  relative  overflow-hidden 
-                    ${i === 0 && "md:row-span-2 lg:h-auto h-60 row-start-1 "}
-                    ${i === 1 && "lg:h-[280px] h-64 "}
-                    ${i === 2 && "lg:h-80 h-64 "}
-                    ${i === 3 && "lg:h-[330px] h-64 lg:-mt-9 "}
-                    ${i === 4 && "h-72"}
-                    `}
-                  >
-                    <Link href={`blog/${posts[i].slug}`}>
-                      <img
-                        className="h-full  w-full object-cover"
-                        src={posts[i].blog_img_url}
-                        alt=""
-                      />
+           <div
+             key={i}
+             className={`rounded-xl border border-neutral-800 bg-neutral-900  relative  overflow-hidden 
+             ${i === 0 && "md:row-span-2 lg:h-auto h-60 row-start-1 "}
+             ${i === 1 && "lg:h-[280px] h-64 "}
+             ${i === 2 && "lg:h-80 h-64 "}
+             ${i === 3 && "lg:h-[330px] h-64 lg:-mt-9 "}
+             ${i === 4 && "h-72"}
+             `}
+           >
+             <Link href={`blog/${posts[i].slug}`}>
+               <img
+                 className="h-full  w-full object-cover"
+                 src={posts[i].blog_img_url}
+                 alt=""
+               />
 
-                      <p className="text-neutral-50 bottom-14 font-InterBold text-sm lg:text-xl ml-2  absolute z-20 ">
-                        {posts[i].title}
-                      </p>
+               <p className="text-neutral-50 bottom-14 font-InterBold text-sm lg:text-xl ml-2  absolute z-20 ">
+                 {posts[i].title}
+               </p>
 
-                      <div className="absolute bottom-3 z-20 flex justify-between items-center w-full px-2 font-InterMedium text-neutral-500 text-sm">
-                        <div className="flex items-center gap-x-2">
-                          <img
-                            className="w-6 h-6 rounded-full object-cover"
-                            src={posts[i].author_img_url}
-                            alt={posts[i].author_name}
-                          />
-                          <p>  {posts[i].author_name}</p>
-                        </div>
-                        <p> {posts[i].blog_date}</p>
-                      </div>
-                    </Link>
-                    <div className="bg-gradient-to-t  w-full absolute z-10  from-[#000000] via-black/80  to-transparent bottom-0   h-44 transition-all ease-in duration-200" />
-                  </div>
-                )
-            )}
+               <div className="absolute bottom-3 z-20 flex justify-between items-center w-full px-2 font-InterMedium text-neutral-500 text-sm">
+                 <div className="flex items-center gap-x-2">
+                   <img
+                     className="w-6 h-6 rounded-full object-cover"
+                     src={posts[i].author_img_url}
+                     alt={posts[i].author_name}
+                   />
+                   <p>  {posts[i].author_name}</p>
+                 </div>
+                 <p> {posts[i].blog_date}</p>
+               </div>
+             </Link>
+             <div className="bg-gradient-to-t  w-full absolute z-10  from-[#000000] via-black/80  to-transparent bottom-0   h-44 transition-all ease-in duration-200" />
+           </div>
+         )
+           )}
+        </>
+      ) : (
+      <>
+       {[...Array(5)].map((_, i) => (
+
+<div
+  key={i}
+  className={`rounded-xl border border-neutral-800 bg-neutral-900  relative  overflow-hidden
+  ${i === 0 && "md:row-span-2 row-start-1 "}
+  ${i === 2 && "lg:h-80 h-64 "}
+  ${i === 1 && "lg:h-[280px] h-64 "}
+  ${i === 3 && "lg:h-[330px] h-64 lg:-mt-9 "}
+  ${i === 4 && "h-72"}
+  `}
+>
+  <Link href='#'>
+    <img
+      className="h-full  w-full object-cover"
+      src='./images/maven.jpg'
+      alt=""
+    />
+
+    <p className="text-neutral-50 bottom-14 font-InterBold text-xl ml-2  absolute z-20 ">
+      Atlas Communications
+    </p>
+
+    <div className="absolute bottom-3 z-20 flex justify-between items-center w-full px-2 font-InterMedium text-neutral-500 text-sm">
+      <div className="flex items-center gap-x-2">
+        <img
+          className="w-6 h-6 rounded-full object-cover"
+          // src="images/img3.jpg"
+          src='./images/troisiemeoeillogo.png'
+          alt=""
+        />
+        <p>Troisieme Oeil</p>
+      </div>
+      <p>21/02/2021</p>
+    </div>
+  </Link>
+  <div className=" bg-gradient-to-t  w-full absolute z-10  from-[#000000] via-black/80  to-transparent bottom-0   h-44 transition-all ease-in duration-200" />
+</div>
+)
+)}
+      </>
+      )}
+    
+    
+          
           </div>
 
           <div className="mt-6 grid lg:grid-cols-2 lg:gap-11 p-3 gap-7  w-4/5">
