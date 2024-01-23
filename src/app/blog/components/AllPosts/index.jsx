29565aa340ab6759de046b0772a0a5a297a633c1
neuronarchitecture27 <troisiemeoeilagency@gmail.com>
2024-01-23 12:34:53 +0100

    adding skel v2

--- src/app/blog/components/AllPosts/index.jsx
+++ src/app/blog/components/AllPosts/index.jsx
@@ -10,17 +10,17 @@ export default function AllPosts() {
   const [posts, setposts] = useState(null)
   const [isReady, setIsReady] = useState(false);
   useEffect(()=> {
-    const getData = async () => {
-      let { data: posts, error } = await supabase
-      .from('blogs')
-      .select()
-      .order('created_at', { ascending: false })
-      setposts(posts);
-      setIsReady(true);
-    console.log(posts);
-
-    }
-    getData()
+      const getData = async () => {
+        let { data: posts, error } = await supabase
+        .from('blogs')
+        .select()
+        .order('created_at', { ascending: false })
+        setposts(posts);
+        setIsReady(true);
+        console.log(posts);
+        
+      }
+      getData()
   },[])
 
   return (
@@ -72,47 +72,86 @@ export default function AllPosts() {
            )}
         </>
       ) : (
+        // Render skeleton if posts NOT ready
+
       <>
        {[...Array(5)].map((_, i) => (
 
-<div
-  key={i}
-  className={`rounded-xl border border-neutral-800 bg-neutral-900  relative  overflow-hidden
-  ${i === 0 && "md:row-span-2 row-start-1 "}
-  ${i === 2 && "lg:h-80 h-64 "}
-  ${i === 1 && "lg:h-[280px] h-64 "}
-  ${i === 3 && "lg:h-[330px] h-64 lg:-mt-9 "}
-  ${i === 4 && "h-72"}
-  `}
->
-  <Link href='#'>
-    <img
-      className="h-full  w-full object-cover"
-      src='./images/maven.jpg'
-      alt=""
-    />
-
-    <p className="text-neutral-50 bottom-14 font-InterBold text-xl ml-2  absolute z-20 ">
-      Atlas Communications
-    </p>
-
-    <div className="absolute bottom-3 z-20 flex justify-between items-center w-full px-2 font-InterMedium text-neutral-500 text-sm">
-      <div className="flex items-center gap-x-2">
-        <img
-          className="w-6 h-6 rounded-full object-cover"
-          // src="images/img3.jpg"
-          src='./images/troisiemeoeillogo.png'
-          alt=""
-        />
-        <p>Troisieme Oeil</p>
-      </div>
-      <p>21/02/2021</p>
-    </div>
-  </Link>
-  <div className=" bg-gradient-to-t  w-full absolute z-10  from-[#000000] via-black/80  to-transparent bottom-0   h-44 transition-all ease-in duration-200" />
-</div>
+            <div
+              key={i}
+              className={`rounded-xl border border-neutral-800 bg-neutral-900  relative  overflow-hidden bg-gradient-to-b from-neutral-800  to-neutral-700
+              ${i === 0 && "md:row-span-2 row-start-1 "}
+              ${i === 2 && "lg:h-80 h-64 "}
+              ${i === 1 && "lg:h-[280px] h-64 "}
+              ${i === 3 && "lg:h-[330px] h-64 lg:-mt-9 "}
+              ${i === 4 && "h-72"}
+              `}
+            >
+           
+              <div className=" bg-gradient-to-t  w-full absolute z-10  from-[#000000] via-black/80  to-transparent bottom-0   h-44 transition-all ease-in duration-200" />
+            </div>
 )
 )}
+
+<div className="mt-6 grid lg:grid-cols-2 lg:gap-11 p-3 gap-7  w-[78vw]">
+        <div className='bg-gradient-to-b from-neutral-800  to-neutral-700 rounded-md'>
+              <Link
+                href='#'
+                className="hover:bg-neutral-950 p-2 hover:rounded-2xl"
+              >
+          
+                <div className="text-neutral-500 flex  w-full items-center ">
+              <div className="flex items-center gap-x-3 h-12 ">
+          
+              
+              </div>
+                </div>
+              </Link>
+        </div>
+        <div className='bg-gradient-to-b from-neutral-800  to-neutral-700 rounded-md'>
+              <Link
+                href='#'
+                className="hover:bg-neutral-950 p-2 hover:rounded-2xl"
+              >
+          
+                <div className="text-neutral-500 flex  w-full items-center ">
+              <div className="flex items-center gap-x-3 h-12 ">
+          
+              
+              </div>
+                </div>
+              </Link>
+        </div>
+        <div className='bg-gradient-to-b from-neutral-800  to-neutral-700 rounded-md'>
+              <Link
+                href='#'
+                className="hover:bg-neutral-950 p-2 hover:rounded-2xl"
+              >
+          
+                <div className="text-neutral-500 flex  w-full items-center ">
+              <div className="flex items-center gap-x-3 h-12 ">
+          
+              
+              </div>
+                </div>
+              </Link>
+        </div>
+        <div className='bg-gradient-to-b from-neutral-800  to-neutral-700 rounded-md'>
+              <Link
+                href='#'
+                className="hover:bg-neutral-950 p-2 hover:rounded-2xl"
+              >
+          
+                <div className="text-neutral-500 flex  w-full items-center ">
+              <div className="flex items-center gap-x-3 h-12 ">
+          
+              
+              </div>
+                </div>
+              </Link>
+        </div>
+  </div>
+
       </>
       )}
     
